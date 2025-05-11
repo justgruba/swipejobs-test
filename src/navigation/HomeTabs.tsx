0641c3f6a95a4f5995c15eb75d8c22adfcd149b8
Justyna Gruba <justyna.gruba@deployed.pl>
2025-05-11 21:54:27 +0200

    Update bottom tab navigation style.

--- src/navigation/HomeTabs.tsx
+++ src/navigation/HomeTabs.tsx
@@ -2,6 +2,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Home from '../screens/Home';
 import Profile from '../screens/Profile';
+import { Icon } from 'react-native-paper';
 
 export type BottomTabsParamList = {
   Home: undefined;
@@ -12,11 +13,28 @@ const BottomTabs = createBottomTabNavigator<BottomTabsParamList>();
 
 const HomeTabs = () => {
   return (
-    <BottomTabs.Navigator>
-      <BottomTabs.Screen name="Home" component={Home} />
-      <BottomTabs.Screen name="Profile" component={Profile} />
+    <BottomTabs.Navigator
+      screenOptions={{ tabBarActiveTintColor: '#30d4ac', tabBarInactiveTintColor: 'gray' }}
+    >
+      <BottomTabs.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon source="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <BottomTabs.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon source="account" color={color} size={size} />
+          ),
+        }}
+      />
     </BottomTabs.Navigator>
   );
 };
-
 export default HomeTabs;
