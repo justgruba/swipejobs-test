c39b6f79b5087a41bbe782dfbcb4c6a8c7de9217
Justyna Gruba <justyna.gruba@deployed.pl>
2025-05-12 08:30:02 +0200

    Headers added to te navigators.

--- src/navigation/AppStack.tsx
+++ src/navigation/AppStack.tsx
@@ -1,27 +1,57 @@
+import { StyleSheet } from 'react-native';
+
 import { NavigationContainer, NavigatorScreenParams } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Text } from 'react-native-paper';
 
-import { JobListing } from '@/api/types';
+import { useUserProfile } from '@/hooks/useUserProfile';
 import JobDetails from '@/screens/JobDetails/JobDetails';
 
 import HomeTabs, { BottomTabsParamList } from './HomeTabs';
 
 export type RootStackParamList = {
   HomeTabs: NavigatorScreenParams<BottomTabsParamList>;
-  JobDetails: { job: JobListing };
+  JobDetails: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const AppStack = () => {
+  const { data } = useUserProfile();
+  const fullName = data ? `${data.firstName} ${data.lastName}` : '';
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: 'black' },
+          headerLeft: () => <HeaderLeft />,
+          headerRight: () => <HeaderRight name={fullName} />,
+        }}
+      >
         <Stack.Screen name="HomeTabs" component={HomeTabs} options={{ headerShown: false }} />
-        <Stack.Screen name="JobDetails" component={JobDetails} />
+        <Stack.Screen name="JobDetails" component={JobDetails} options={{ headerTitle: '' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
+const HeaderLeft = () => (
+  <Text variant="headlineLarge" style={styles.text}>
+    SwipeJobs
+  </Text>
+);
+
+const HeaderRight = ({ name }: { name: string }) => (
+  <Text variant="headlineLarge" style={styles.text}>
+    {name}
+  </Text>
+);
+
 export default AppStack;
+
+const styles = StyleSheet.create({
+  text: {
+    color: 'white',
+  },
+});
--- src/navigation/HomeTabs.tsx
+++ src/navigation/HomeTabs.tsx
@@ -1,8 +1,11 @@
+import { StyleSheet } from 'react-native';
+
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Icon } from 'react-native-paper';
+import { Text } from 'react-native-paper';
 
 import Home from '@/screens/Home';
 import Profile from '@/screens/Profile';
-import { Icon } from 'react-native-paper';
 
 export type BottomTabsParamList = {
   Home: undefined;
@@ -14,27 +17,39 @@ const BottomTabs = createBottomTabNavigator<BottomTabsParamList>();
 const HomeTabs = () => {
   return (
     <BottomTabs.Navigator
-      screenOptions={{ tabBarActiveTintColor: '#30d4ac', tabBarInactiveTintColor: 'gray' }}
+      screenOptions={{
+        tabBarActiveTintColor: '#30d4ac',
+        tabBarInactiveTintColor: 'gray',
+        headerStyle: { backgroundColor: 'black' },
+        headerTitle: () => (
+          <Text variant="headlineLarge" style={styles.text}>
+            SwipeJobs
+          </Text>
+        ),
+        headerTitleAlign: 'center',
+      }}
     >
       <BottomTabs.Screen
         name="Home"
         component={Home}
         options={{
-          tabBarIcon: ({ color, size }) => (
-            <Icon source="home" color={color} size={size} />
-          ),
+          tabBarIcon: ({ color, size }) => <Icon source="home" color={color} size={size} />,
         }}
       />
       <BottomTabs.Screen
         name="Profile"
         component={Profile}
         options={{
-          tabBarIcon: ({ color, size }) => (
-            <Icon source="account" color={color} size={size} />
-          ),
+          tabBarIcon: ({ color, size }) => <Icon source="account" color={color} size={size} />,
         }}
       />
     </BottomTabs.Navigator>
   );
 };
 export default HomeTabs;
+
+const styles = StyleSheet.create({
+  text: {
+    color: 'white',
+  },
+});
